a0182008eaa6072523ffa5b04ff5e51ada580269
Chris haznmo <haznmo@gmail.com>
2024-10-09 11:14:20 +0300

    Add JWT token generation and validation
    Introduces new functions `sigin` and `decode` for generating and validating JWT tokens using FastAPI and PyJWT. This update enhances authentication mechanisms by securely encoding payloads with a secret key.

---
+++ API_construct/new_main.py
@@ -0,0 +1,26 @@
+import time
+import jwt
+from fastapi import HTTPException
+from pydantic import BaseModel
+import secrets
+
+JWT_SECRET = secrets.token_hex(32)
+JWT_ALGORITHM = "HS256"
+
+print(JWT_SECRET)
+
+def sigin(email):
+    payload = {
+        "email": email,
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token
+
+def decode(token):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token
+
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
