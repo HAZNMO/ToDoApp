da60ba64133c2031df8c8b67a95d8d94a4fe4045
Chris haznmo <haznmo@gmail.com>
2024-10-22 19:01:33 +0300

    Rename 'to_dos_router' to 'todos_router'
    Updated the variable name from 'to_dos_router' to 'todos_router' for consistency and readability in both the router inclusion and within the todos module. This change affects the import statements and the routes' definitions in the todos module.

--- to_do_app/API/v1/todos.py
+++ to_do_app/API/v1/todos.py
@@ -6,14 +6,14 @@ from to_do_app.domains.todos.flow import get_todos
 from to_do_app.domains.todos.service import create_todo, update_todo, delete_todo
 from to_do_app.domains.todos.schemas import TodoModel, UpdateTODOModel, TaskStatus, CreateTodoModel
 
-to_dos_router = APIRouter(prefix="/todos")
-@to_dos_router.get("/get_todos", response_model=list[TodoModel])
+todos_router = APIRouter(prefix="/todos")
+@todos_router.get("/get_todos", response_model=list[TodoModel])
 async def get_todos_route(
     task_status: Optional[TaskStatus] = Query(None, description="Task status to filter by (To Do, In Progress, Done)"),
     user_info: dict = Depends(decode_token)):
     return await get_todos(user_info=user_info, task_status=task_status)
 
-@to_dos_router.post("/create_todos",
+@todos_router.post("/create_todos",
           response_description="Add new to do",
           response_model=CreateTodoModel,
           status_code=status.HTTP_201_CREATED,
@@ -21,13 +21,13 @@ async def get_todos_route(
 async def create_todo_route(todo: CreateTodoModel = Body(...), user_info: dict = Depends(decode_token)):
     return await create_todo(todo, user_info)
 
-@to_dos_router.put("/todos/{todo_id}",
+@todos_router.put("/todos/{todo_id}",
          response_description="Update a to do",
          response_model=UpdateTODOModel,
          response_model_by_alias=False)
 async def update_todo_route(todo_id: str, todo_update: UpdateTODOModel = Body(...), user_info: dict = Depends(decode_token)):
     return await update_todo(todo_id,todo_update,user_info)
 
-@to_dos_router.delete("/todos/{todo_id}", response_description="Delete a to do")
+@todos_router.delete("/todos/{todo_id}", response_description="Delete a to do")
 async def delete_todo_route(todo_id: str, user_info: dict = Depends(decode_token)):
     return await delete_todo(todo_id,user_info)
