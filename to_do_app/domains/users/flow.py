a5b7e7ed940354208b72143c83a5757efbefa469
Chris haznmo <haznmo@gmail.com>
2024-10-25 14:21:05 +0300

    Refactor user registration to use UserCreate schema, and UserLogin schema
    - Updated the `register_user` function to accept a `UserCreate` schema instead of individual parameters (name, email, password).
    - Updated the `authenticate_user` function to accept a `UserLogin` schema instead of
    individual parameters (name, email, password).
    - Modified the `register` function to return a `UserResponse` with both email and token fields.
    - Modified the `login` function to return a `UserResponse` with both email and token fields.
    - Ensured that the user email is passed to `UserResponse` during registration.

--- to_do_app/domains/users/flow.py
+++ to_do_app/domains/users/flow.py
@@ -1,10 +1,10 @@
 from to_do_app.domains.users.service import authenticate_user, register_user
 from to_do_app.domains.users.schemas import UserCreate, UserLogin, UserResponse
 
-async def register(user: UserCreate):
-    token = await register_user(user.name, user.email, user.password)
-    return UserResponse(email=user.email, token=token)
+async def register(context: UserCreate):
+    token = await register_user(context)
+    return UserResponse(email=context.email, token=token)
 
-async def login(user: UserLogin):
-    token = await authenticate_user(user.email, user.password)
-    return UserResponse(email=user.email, token=token)
+async def login(context: UserLogin):
+    token = await authenticate_user(context)
+    return UserResponse(email=context.email, token=token)
--- to_do_app/domains/users/service.py
+++ to_do_app/domains/users/service.py
@@ -1,31 +1,35 @@
 from to_do_app.Infrastructure.DB.mongo_db.mongo_construct import user_collection
 from to_do_app.dependences.auth.dependeces import hash_password, verify_password, create_token
+from to_do_app.domains.users.schemas import UserCreate, UserLogin
 from fastapi import HTTPException
 from datetime import datetime
 
 
-async def register_user(name: str, email: str, password: str):
-    user_exists = await user_collection.find_one({"email": email})
+async def register_user(user_create: UserCreate):
+    user_exists = await user_collection.find_one({"email": user_create.email})
     if user_exists:
         raise HTTPException(status_code=400, detail="Email already registered")
-    hashed_password = hash_password(password)
+
+    hashed_password = hash_password(user_create.password)
     new_user = {
-        "name": name,
-        "email": email,
+        "name": user_create.name,
+        "email": user_create.email,
         "password": hashed_password,
         "created_at": datetime.now()
     }
+
     result = await user_collection.insert_one(new_user)
 
     if result.inserted_id is None:
         raise HTTPException(status_code=500, detail="Failed to register user")
 
     user_id = str(result.inserted_id)
-    return create_token(user_id, email)
+    return create_token(user_id, user_create.email)
 
-async def authenticate_user(email: str, password: str):
-    user = await user_collection.find_one({"email": email})
-    if user and verify_password(password, user["password"]):
+
+async def authenticate_user(user_login: UserLogin):
+    user = await user_collection.find_one({"email": user_login.email})
+    if user and verify_password(user_login.password, user["password"]):
         user_id = str(user["_id"])
-        return create_token(user_id, email)
+        return create_token(user_id, user_login.email)
     raise HTTPException(status_code=400, detail="Incorrect email or password")
